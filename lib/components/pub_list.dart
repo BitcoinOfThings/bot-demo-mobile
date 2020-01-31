5de12446cbac43f98488a37f3ac5d4092d270f4c
david foderick <dfoderick@gmail.com>
2020-01-31 08:03:21 -0800

    intro basepubsub model

--- lib/components/pub_list.dart
+++ lib/components/pub_list.dart
@@ -5,17 +5,25 @@ class PublicationTile extends StatelessWidget {
   final Publication _pub;
   PublicationTile(this._pub);
 
+  _valchanged(val) {
+    this._pub.enabled = val;
+    this._pub.subscribe();
+  }
+
   @override
   Widget build(BuildContext context) => Column(
     children: <Widget>[
-      ListTile(
+      SwitchListTile(
         title: Text(_pub.name),
-        subtitle: Text(_pub.description),
+        //subtitle: Text(_sub.description),
+        value: this._pub?.enabled,
+        onChanged: (bool value) { _valchanged(value); },
         // leading: Container(
         //   margin: EdgeInsets.only(left: 6.0),
         //   child: Image.network(_pub.icon, height: 50.0, fit: BoxFit.fill,)
         // ),
       ),
+
       Divider()
     ],
   );
--- lib/models/Subscription.dart
+++ lib/models/Subscription.dart
@@ -1,65 +1,20 @@
-import '../mqtt_stream.dart';
+import 'package:bot_demo_mobile/models/pubsub_base.dart';
 
-class Subscription {
-  // wss:// is for mqtt over websockets
-  // final String server = 'wss://mqtt.bitcoinofthings.com';
-  // final int port = 8884;
-  final String server = 'mqtt.bitcoinofthings.com';
-  final int port = 1883;
-  final String id;
-  final String username;
-  final String topic;
+
+class Subscription extends BasePubSub {
+  //final String topic;
   final String name;
   final String status;
   //will be date time
   String expires;
-  // flag managed in UI subs view
-  bool _enabled = false;
-
-  bool get enabled { return this._enabled;}
-
-  // this setter should not subscribe! fix later
-  set enabled (bool val) {
-      this._enabled = val;
-   }
 
   String get clientId { return this.id;}
 
-  // the mqtt stream of data
-  PubSubConnection _pubsub;
-
-  set pubsub (PubSubConnection ps) { this._pubsub = ps;}
-
-  Subscription(this.id, this.username, this.topic, this.name, this.status);
+  Subscription(String id, String username, String topic, this.name, this.status) : 
+    super(id, username, topic);
 
   Subscription.fromJSON(Map<String, dynamic> json) :
-    id = json['clientId'],
-    username = json['username'],
-    topic = json['pub']['topic'],
     name = json['pub']['name'],
-    status = json['status'];
-
-  subscribe() async {
-      try {
-      print('start/stop sub here ${this.enabled}');
-      if (_pubsub != null) {
-        if (!this.enabled) {
-          if ( _pubsub == null) {
-            print('SORRY! PUBSUB IS NULL. WILL NOT UNSUBSCRIBE!');
-          } else {
-            await _pubsub.unsubscribe(this.topic);
-          }
-        }
-        if (this.enabled) {
-          await _pubsub.subscribe(this.topic);
-        }
-      }
-      //all went well, complete the setter
-    }
-    catch (err) {
-      // todo show error
-      // leaves value unchanged so ui will revert
-    }
-
-  }
+    status = json['status'], 
+    super(json['clientId'], json['username'], json['pub']['topic']);
 }
