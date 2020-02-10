cd2aad8a8ee1d2db6d6c6f68c234e80efc955644
fullcycle <dfoderick@gmail.com>
2020-02-09 16:25:42 -0800

    try get notifications working on ios

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -7,6 +7,11 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+  // following iOS Integration instructions from 
+  // https://pub.dev/packages/flutter_local_notifications
+  if (@available(iOS 10.0, *)) {
+    [UNUserNotificationCenter currentNotificationCenter].delegate = (id<UNUserNotificationCenterDelegate>) self;
+  }
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
